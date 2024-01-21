cfb685a4a11e150ab83ea0e013db2e914e496c8f
Mikhaylyuk Sergey <s.mikhaylyuk@hacken.io>
2024-01-21 22:24:04 +0000

    done work

---
+++ index.js
@@ -0,0 +1,36 @@
+const { program } = require('commander');
+require('colors');
+
+const { listContacts, getContactById, removeContact, addContact } = require('./src/contacts');
+
+program
+	.option("-a, --action <type>", "choose action")
+	.option("-i, --id <type>", "user id")
+	.option("-n, --name <type>", "user name")
+	.option("-e, --email <type>", "user email")
+	.option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+async function invokeAction() {
+	switch (options.action) {
+		case 'list':
+			console.log(await listContacts());
+			break;
+		case 'get':
+			console.log(await getContactById(options.id));
+			break;
+		case 'remove':
+			console.log(await removeContact(options.id));
+			break;
+		case 'add':
+			console.log(await addContact(options.name, options.email, options.phone));
+			break;
+		default:
+			console.log('Invalid action');
+	}
+}
+
+invokeAction().then();
